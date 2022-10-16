e056ca01c64b9f2e148d205287a9229debe4afb7
Leonid <leon9520071@gmail.com>
2022-10-16 19:36:51 +0300

    Знакомство с языками программирования HM1

--- Program.cs
+++ Program.cs
@@ -182,15 +182,14 @@ else
 */
 
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
-
-
+/*
 Console.Write("Input a number: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int i = 1;
 
-for (i = 1; i <= n; i++);
+for (i = 1; i <= n; i++)
 
     if (i % 2 == 0)
     
         Console.Write(i + " ");
-  
+*/
